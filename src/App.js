0323dd09edcd1fc5d8b2423b40378f0bb251ba5d
Thinhlal273 <thinhlal273@gmail.com>
2024-05-21 19:27:50 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import { Routes, Route, Link } from 'react-router-dom'
+// import SignUp from './SignUp.js';
+import Booking from './Booking.js';
+import Home from './Home.js';
+import SignUp from './SignUp.js';
+
+function App() {
+  return (
+    <div className="App">
+      <SignUp></SignUp>
+    </div>
+  );
+}
+
+export default App;
