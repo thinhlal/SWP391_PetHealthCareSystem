773bee45c3bcab8809187530043f4006fe146947
Minh <anhminh0026@gmail.com>
2024-05-28 19:12:33 +0700

    fix_add_animation_services_page

--- src/pages/Services/Services.js
+++ src/pages/Services/Services.js
@@ -1,4 +1,7 @@
-import "./Services.css";
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import './Services.css';
 
 // components
 import Footer from "../../components/User/Footer/Footer.js";
@@ -10,137 +13,138 @@ import Tick from "../../assets/images/img_Services/rectangle-1-7.png";
 import Dog from "../../assets/images/img_Services/rectangle-1-4.png";
 
 function Services() {
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
   return (
     <div className="all-services">
       <Header></Header>
       <div className="services-slider">
-        <div className="overlap-group">
-          <p className="p">The highest standards of pet care</p>
+        <div className="overlap-services-slider">
+          <div className="title-services-slider" data-aos="fade-down">
+            The highest standards of pet care
+          </div>
         </div>
       </div>
 
       <div className="main-tittle">
-        <p className="text-wrapper-3">Choose The Best Care For Yourself</p>
+        <div className="main-title-text" data-aos="fade-right">Choose The Best Care For Yourself</div>
       </div>
 
       {/* Cat Part */}
-      <div className="cat-part">
+      <div className="cat-part" data-aos="fade-left">
         <div className="picture-services-cat">
           <div className="avatar-cat">
-            {/* image cat */}
-            <img className="rectangle" src={Cat} alt="cat" />
+            <img className="photo-frame-img-service" src={Cat} alt="cat" />
           </div>
         </div>
         <div className="content-services-cat">
           <div className="list-item-cat">
-            <div className="text-wrapper-7">Cat Services</div>
-            <div className="item">
-              <div className="avatar">
-                <img className="rectangle-2" src={Tick} alt="avatar" />
+            <div className="title-services-cat">Cat Services</div>
+
+            <div className="content-list-services-cat">
+              <div className="green-tick-image">
+                <img className="green-tick-src-image" src={Tick} alt="avatar" />
               </div>
-              <div className="text">
-                <div className="text-wrapper-4">Helena Hills</div>
-                <p className="text-wrapper-cat">
-                  Staff learn proper handling, signs, and symptoms of illness,
-                  dog expressions, and body language.
-                </p>
-              </div>
-            </div>
-            <div className="item-2">
-              <div className="avatar">
-                <img className="rectangle-2" src={Tick} alt="avatar" />
-              </div>
-              <div className="text">
-                <div className="text-wrapper-4">Charles Tran</div>
-                <p className="text-wrapper-cat">
-                  Snacks are available throughout the day and fresh water is
-                  constantly available.
-                </p>
+              <div className="text-content-services-cat">
+                <div className="title-text-content-services-cat">Routine Health Check-ups</div>
+                <div className="text-wrapper-cat">
+                  Ensure your cat gets regular general health check-ups to
+                  detect potential issues early.
+                </div>
               </div>
             </div>
-            <div className="item">
-              <div className="avatar">
-                <img className="rectangle-2" src={Tick} alt="avatar" />
+
+            <div className="content-list-services-cat">
+              <div className="green-tick-image">
+                <img className="green-tick-src-image" src={Tick} alt="avatar" />
               </div>
-              <div className="text">
-                <div className="text-wrapper-4">Oscar Davis</div>
-                <p className="text-wrapper-cat">
-                  Snacks are available throughout the day and fresh water is
-                  constantly available.
-                </p>
+              <div className="text-content-services-cat">
+                <div className="title-text-content-services-cat">Vaccinations</div>
+                <div className="text-wrapper-cat">
+                  Protect your cat from infectious diseases such as cat flu,
+                  rabies, and others through proper vaccination.
+                </div>
+              </div>
+            </div>
+
+            <div className="content-list-services-cat">
+              <div className="green-tick-image">
+                <img className="green-tick-src-image" src={Tick} alt="avatar" />
+              </div>
+              <div className="text-content-services-cat">
+                <div className="title-text-content-services-cat">Surgery</div>
+                <div className="text-wrapper-cat">
+                  Perform necessary surgeries such as spaying/neutering,
+                  emergency surgeries, and other operations.
+                </div>
               </div>
             </div>
             <button
-              className="element-primary-button"
+              className="booking-services-button"
               onClick={() => (window.location.href = "booking")}
               style={{ border: "none" }}
             >
-              <div className="text-wrapper-6">Book now</div>
+              <div className="text-booking-services-button">Book now</div>
             </button>
           </div>
         </div>
       </div>
 
       {/* Dog Part */}
-      <div className="dog-part">
+      <div className="dog-part" data-aos="fade-right">
         <div className="content-services-dog">
           <div className="list-services-dog">
             <div className="list-services-dog-title">Dog Services</div>
             <div className="list-services-dog-item">
               <div className="list-services-dog-item-text">
-                <div className="list-services-dog-item-text-name">
-                  Oscar Davis
-                </div>
+                <div className="list-services-dog-item-text-name">Routine Health Check-ups</div>
                 <div className="list-services-dog-item-text-desc">
-                  Snacks are available throughout the day and fresh water is
-                  constantly available.
+                  Ensure your dog gets regular general health check-ups to
+                  detect potential issues early.
                 </div>
               </div>
-              <div className="avatar">
-                <img className="rectangle-2" src={Tick} alt="avatar" />
+              <div className="green-tick-image">
+                <img className="green-tick-src-image" src={Tick} alt="avatar" />
               </div>
             </div>
             <div className="list-services-dog-item">
               <div className="list-services-dog-item-text">
-                <div className="list-services-dog-item-text-name">
-                  Charles Tran
-                </div>
+                <div className="list-services-dog-item-text-name">Vaccinations</div>
                 <div className="list-services-dog-item-text-desc">
-                  Snacks are available throughout the day and fresh water is
-                  constantly available.
+                  Protect your dog from infectious diseases such as rabies,
+                  Parvo, and others through proper vaccination.
                 </div>
               </div>
-              <div className="avatar">
-                <img className="rectangle-2" src={Tick} alt="avatar" />
+              <div className="green-tick-image">
+                <img className="green-tick-src-image" src={Tick} alt="avatar" />
               </div>
             </div>
             <div className="list-services-dog-item">
               <div className="list-services-dog-item-text">
-                <div className="list-services-dog-item-text-name">
-                  Charles Tran
-                </div>
+                <div className="list-services-dog-item-text-name">Surgery</div>
                 <div className="list-services-dog-item-text-desc">
-                  Snacks are available throughout the day and fresh water is
-                  constantly available.
+                  Perform necessary surgeries such as spaying/neutering,
+                  emergency surgeries, and other operations.
                 </div>
               </div>
-              <div className="avatar">
-                <img className="rectangle-2" src={Tick} alt="avatar" />
+              <div className="green-tick-image">
+                <img className="green-tick-src-image" src={Tick} alt="avatar" />
               </div>
             </div>
             <button
-              className="element-primary-button"
+              className="booking-services-button"
               onClick={() => (window.location.href = "booking")}
               style={{ border: "none" }}
             >
-              <div className="text-wrapper-6">Book now</div>
+              <div className="text-booking-services-button">Book now</div>
             </button>
           </div>
         </div>
         <div className="picture-services-dog">
           <div className="avatar-dog">
-            {/* image dog */}
-            <img className="rectangle" src={Dog} alt="dog" />
+            <img className="photo-frame-img-service" src={Dog} alt="dog" />
           </div>
         </div>
       </div>
