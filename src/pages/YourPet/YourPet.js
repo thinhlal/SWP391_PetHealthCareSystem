ebddc048330628e43d08c2939e7dcc48eecfa9c8
thinhlal <thinhlal273@gmail.com>
2024-06-04 21:13:30 +0700

    CreateSideBarUserComponent And Fix Manage Your Booking "Choose Doctor"

--- src/pages/YourPet/YourPet.js
+++ src/pages/YourPet/YourPet.js
@@ -10,7 +10,7 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import pet_img1 from '../../assets/images/img_YourPet/c2dc9a5328014cead97d6268b688a16e.jpg';
 import pet_img2 from '../../assets/images/img_YourPet/30c8eac7d84112a145ab7b06ce9e6eb1.jpg';
 import pet_img3 from '../../assets/images/img_YourPet/f86eb143541c5a58b7132ab54d6d1f12.jpg';
-import dog_icon from '../../assets/images/img_YourPet/dog_icon.png';
+import Sidebar from "../../components/User/Sidebar/Sidebar.js";
 function YourPet() {
     const [avatar, setAvatar] = useState();
     useEffect(() => {
@@ -27,7 +27,7 @@ function YourPet() {
         }
     }
     return (
-        <div className="container-fluid">
+        <div className="container-fluid your-pet">
             <div className="row">
                 <Header></Header>
                 <div className="main-tittle">
@@ -36,41 +36,7 @@ function YourPet() {
                     </div>
                 </div>
                 <div className="overlap">
-
-                    <div className="your-pet-sidebar">
-                        <div>
-                            <div className="category-Header_wrapper">
-                                <div className="category-Header">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#000" className="bi bi-list" viewBox="0 0 16 16">
-                                        <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
-                                    </svg>
-                                    <div className="category-Header-Txt">Categories</div>
-                                </div>
-                            </div>
-                            <div className="list-items-category">
-                                <div className="menu-item">
-                                    <img className="menu-icons" src={dog_icon} alt="" />
-                                    <div className="menu-item_txt">Your pet</div>
-                                </div>
-
-                                <div className="menu-item">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-calendar-check" viewBox="0 0 16 16">
-                                        <path d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0" />
-                                        <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z" />
-                                    </svg>
-                                    <div className="menu-item_txt">Your booking</div>
-                                </div>
-
-                                <div className="menu-item">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house-door" viewBox="0 0 16 16">
-                                        <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4z" />
-                                    </svg>
-                                    <div className="menu-item_txt">Pet are staying in cages</div>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
+                    <Sidebar></Sidebar>
                     <div className="main-content">
                         <div className="your-Pet-Header">
                             <div className="search-pet">
