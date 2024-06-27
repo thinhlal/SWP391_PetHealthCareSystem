a354b0ea2050313f049dda000f833d15eb6d5601
Minh <anhminh0026@gmail.com>
2024-06-28 02:28:04 +0700

    pet profile view button

--- src/pages/ProfilePet/ProfilePet.js
+++ src/pages/ProfilePet/ProfilePet.js
@@ -11,6 +11,8 @@ import axiosInstance from '../../utils/axiosInstance.js';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { storage } from '../../config/firebase.js';
 import { AuthContext } from '../../context/AuthContext.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function ProfilePet() {
   const { user } = useContext(AuthContext);
@@ -23,7 +25,6 @@ function ProfilePet() {
     birthday: '',
     image: '',
   });
-
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -115,7 +116,7 @@ function ProfilePet() {
             <div className='sub-title-info-pet'>
               <strong>Pet Parent</strong>{' '}
             </div>
-            <div>{petData.customerDetails[0].username}</div>
+            <div className='customer-name-title'>{petData.customerDetails[0].username}</div>
             <div className='sub-title-info-pet'>
               <strong>ID:&nbsp;</strong>{' '}
               <span className='ID-pet-profile'>{petData.petID}</span>
@@ -124,14 +125,16 @@ function ProfilePet() {
         </div>
         <div className='main-content-pet'>
           <div className='pet-info'>
-            <div className='main-edit-button-pet-info'>
-              <button
-                onClick={handleEditClick}
-                className='edit-button-pet-info'
-              >
-                Edit
-              </button>
-            </div>
+            {!isEditing && (
+              <div className='main-edit-button-pet-info'>
+                <button
+                  onClick={handleEditClick}
+                  className='edit-button-pet-info'
+                >
+                  Edit
+                </button>
+              </div>
+            )}
             <div className='pet-details-profile'>
               <div className='title-pet-profile'>
                 <span className='sub-title-info-pet'>Name:&nbsp;</span> {petData.name}
@@ -211,6 +214,7 @@ function ProfilePet() {
                   <th>Treatment</th>
                   <th>Prescription</th>
                   <th>Notes</th>
+                  <th>More</th>
                 </tr>
               </thead>
               <tbody>
@@ -226,6 +230,38 @@ function ProfilePet() {
                       <td>{reportInfo.treatment}</td>
                       <td>{reportInfo.prescription}</td>
                       <td>{reportInfo.notes}</td>
+                      <td>
+                        <button
+                          type="button"
+                          className="btn btn-primary"
+                          data-bs-toggle="modal"
+                          data-bs-target={`#staticBackdropMedical-${index}`}
+                        >
+                          View
+                        </button>
+
+                        {/* Modal */}
+                        <div className="modal fade" id={`staticBackdropMedical-${index}`} data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                          <div className="modal-dialog">
+                            <div className="modal-content">
+                              <div className="modal-header">
+                                <h5 className="modal-title" id="staticBackdropLabel">Medical Report Details</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                              </div>
+                              <div className="modal-body">
+                                <p><strong>Medical ReportID:</strong> {reportInfo.medicalReportID}</p>
+                                <p><strong>Diagnosis:</strong> {reportInfo.diagnosis}</p>
+                                <p><strong>Treatment:</strong> {reportInfo.treatment}</p>
+                                <p><strong>Prescription:</strong> {reportInfo.prescription}</p>
+                                <p><strong>Notes:</strong> {reportInfo.notes}</p>
+                              </div>
+                              <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </td>
                     </tr>
                   ))
                 )}
@@ -239,9 +275,10 @@ function ProfilePet() {
               <thead>
                 <tr>
                   <th>Date Given</th>
-                  <th>Vaccines</th>
+                  <th className='vaccines-column'>Vaccines</th>
                   <th>Next Vaccination Date</th>
                   <th>Vaccination Notes</th>
+                  <th>More</th>
                 </tr>
               </thead>
               <tbody>
@@ -256,6 +293,37 @@ function ProfilePet() {
                       <td>{vaccine.vaccinationDetails.name}</td>
                       <td>{`${parseInt(vaccine.dateGiven.split('-')[0]) + vaccine.vaccinationDetails.nextDate}`}</td>
                       <td>{vaccine.vaccinationDetails.notes}</td>
+                      <td>
+                        <button
+                          type="button"
+                          className="btn btn-primary"
+                          data-bs-toggle="modal"
+                          data-bs-target={`#staticBackdropVaccine-${index}`}
+                        >
+                          View
+                        </button>
+
+                        {/* Modal */}
+                        <div className="modal fade" id={`staticBackdropVaccine-${index}`} data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                          <div className="modal-dialog">
+                            <div className="modal-content">
+                              <div className="modal-header">
+                                <h5 className="modal-title" id="staticBackdropLabel">Vaccination Details</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                              </div>
+                              <div className="modal-body">
+                                <p><strong>Date Given:</strong> {`${vaccine.dateGiven.split('T')[0]} ${vaccine.dateGiven.split('T')[1].split('.')[0]}`}</p>
+                                <p><strong>Vaccines:</strong> {vaccine.vaccinationDetails.name}</p>
+                                <p><strong>Next Vaccination Date:</strong> {`${parseInt(vaccine.dateGiven.split('-')[0]) + vaccine.vaccinationDetails.nextDate}`}</p>
+                                <p><strong>Vaccination Notes:</strong> {vaccine.vaccinationDetails.notes}</p>
+                              </div>
+                              <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </td>
                     </tr>
                   ))
                 )}
