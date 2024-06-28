e8106d0ce2fd3e2e6183975435345ab29773959d
thinhlal <thinhlal273@gmail.com>
2024-06-28 12:37:51 +0700

    Create Admin Schema

---
+++ back_end/src/app/models/Admin.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AdminSchema = new Schema(
+  {
+    adminID: {
+      type: String,
+      required: true,
+      maxlength: 8,
+      unique: true,
+    },
+    accountID: {
+      type: String,
+      required: true,
+      maxlength: 8,
+      unique: true,
+      ref: 'Account',
+    },
+    name: {
+      type: String,
+      required: true,
+      maxlength: 50,
+    },
+    phone: {
+      type: Number,
+    },
+    email: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model('Admin', AdminSchema);
