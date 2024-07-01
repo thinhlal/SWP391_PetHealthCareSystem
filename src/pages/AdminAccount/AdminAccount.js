af72d5d51b8bdccf1c817a952ab67c197c82eab1
Dat <nguyentruongdat597@gmail.com>
2024-07-01 16:06:45 +0700

    Admin Revenue Components

--- src/pages/AdminAccount/AdminAccount.js
+++ src/pages/AdminAccount/AdminAccount.js
@@ -1,6 +1,6 @@
 import './AdminAccount.css';
 // React
-import React, { useState, useEffect, useRef, useMemo, useContext } from 'react';
+import React, { useState, useRef, useContext } from 'react';
 // Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
@@ -8,6 +8,7 @@ import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.min';
 // Img
 import logo_pet_health_care from '../../assets/images/img_AdminAccount/logo_pethealthcare.png';
 import icon_search from '../../assets/images/img_AdminAccount/icon_search.svg';
+import Statistic from '../../components/Admin/Statistics/Statistics';
 // MUI
 import BorderColorOutlinedIcon from '@mui/icons-material/BorderColorOutlined';
 import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
@@ -20,12 +21,9 @@ import { AuthContext } from '../../context/AuthContext';
 
 function AdminAccount() {
   const { logOut } = useContext(AuthContext);
-  const [selectedDate, setSelectedDate] = useState('');
   const [search, setSearch] = useState('');
   const [roleFilter, setRoleFilter] = useState('All');
   const [currentAccount, setCurrentAccount] = useState(null);
-  const [filteredRevenueData, setFilteredRevenueData] = useState(null);
-  const [yesterdayRevenueData, setYesterdayRevenueData] = useState(null);
   const [accountData, setAccountData] = useState([
     {
       id: 1,
@@ -79,36 +77,7 @@ function AdminAccount() {
     },
   ]);
 
-  const dailyRevenueData = useMemo(
-    () => [
-      {
-        id: 1,
-        date: '2024-06-21',
-        money: 1200,
-      },
-      {
-        id: 2,
-        date: '2024-06-22',
-        money: 1500,
-      },
-      {
-        id: 3,
-        date: '2024-06-23',
-        money: 1600,
-      },
-      {
-        id: 4,
-        date: '2024-06-24',
-        money: 1200,
-      },
-      {
-        id: 5,
-        date: '2024-06-25',
-        money: 1800,
-      },
-    ],
-    [],
-  );
+
 
   const [newAccount, setNewAccount] = useState({
     user_name: '',
@@ -131,34 +100,6 @@ function AdminAccount() {
   const [errors, setErrors] = useState({});
   const modalRef = useRef(null);
 
-  const handleDateChange = event => {
-    setSelectedDate(event.target.value);
-  };
-
-  useEffect(() => {
-    const today = new Date();
-    const formattedDate = today.toISOString().substr(0, 10);
-    setSelectedDate(formattedDate);
-  }, []);
-
-  useEffect(() => {
-    if (selectedDate) {
-      const filteredData = dailyRevenueData.find(
-        daily => daily.date === selectedDate,
-      );
-      setFilteredRevenueData(filteredData);
-
-      const yesterday = new Date(
-        new Date(selectedDate).setDate(new Date(selectedDate).getDate() - 1),
-      )
-        .toISOString()
-        .substr(0, 10);
-      const yesterdayData = dailyRevenueData.find(
-        daily => daily.date === yesterday,
-      );
-      setYesterdayRevenueData(yesterdayData);
-    }
-  }, [selectedDate, dailyRevenueData]);
 
   const handleRoleFilterChange = event => {
     setRoleFilter(event.target.value);
@@ -305,16 +246,7 @@ function AdminAccount() {
     return matchesRole && matchesSearch;
   });
 
-  const calculatePercentChange = () => {
-    if (filteredRevenueData && yesterdayRevenueData) {
-      const change =
-        ((filteredRevenueData.money - yesterdayRevenueData.money) /
-          yesterdayRevenueData.money) *
-        100;
-      return change.toFixed(2);
-    }
-    return null;
-  };
+
 
   return (
     <div className='Admin-Account container-fluid'>
@@ -418,142 +350,7 @@ function AdminAccount() {
           </div>
 
           <div className='Admin-Account-Main col-md-10'>
-            <div className='Admin-Account-Main_Title'>
-              <div className='Admin-Account-Main_Title-Left'>
-                <h2 className='Admin-Account-Main_Title-Left-Intro'>
-                  {' '}
-                  Hi, welcome back!{' '}
-                </h2>
-                <p className='Admin-Account-Main_Title-Left-text'>
-                  {' '}
-                  Sales monitoring dashboard template.{' '}
-                </p>
-              </div>
-              <div className='Admin-Account-Main_Title-Right'>
-                <label className='Admin-Account-title-Star'>
-                  {' '}
-                  Customer Ratings{' '}
-                </label>
-                <div className='Admin-Account-Star'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    width='12'
-                    height='16'
-                    fill='#fbbc0b'
-                    className='bi bi-star-fill'
-                    viewBox='0 0 16 16'
-                  >
-                    <path d='M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z' />
-                  </svg>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    width='12'
-                    height='16'
-                    fill='#fbbc0b'
-                    className='bi bi-star-fill'
-                    viewBox='0 0 16 16'
-                  >
-                    <path d='M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z' />
-                  </svg>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    width='12'
-                    height='16'
-                    fill='#97a3b9'
-                    className='bi bi-star-fill'
-                    viewBox='0 0 16 16'
-                  >
-                    <path d='M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z' />
-                  </svg>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    width='12'
-                    height='16'
-                    fill='#97a3b9'
-                    className='bi bi-star-fill'
-                    viewBox='0 0 16 16'
-                  >
-                    <path d='M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z' />
-                  </svg>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    width='12'
-                    height='16'
-                    fill='#97a3b9'
-                    className='bi bi-star-fill'
-                    viewBox='0 0 16 16'
-                  >
-                    <path d='M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z' />
-                  </svg>
-                  <span>(14.000)</span>
-                </div>
-              </div>
-            </div>
-
-            <div className='Admin-Account-Main-ChooseDate'>
-              <div className='Admin-Account-Main-ChooseDate_Text'>
-                {' '}
-                Choose Date:{' '}
-              </div>
-              <input
-                type='date'
-                onChange={handleDateChange}
-                value={selectedDate}
-              />
-            </div>
-
-            {filteredRevenueData && (
-              <div className='Admin-Account-Main-Header row'>
-                <div className='Admin-Account-Main-Header-Income col-md-3'>
-                  <div className='Admin-Account-Main-Header-Note'>
-                    {' '}
-                    Daily income{' '}
-                  </div>
-                  <div className='Admin-Account-Main-Header-Money'>
-                    {' '}
-                    ${filteredRevenueData.money}{' '}
-                  </div>
-                  <div className='Admin-Account-Main-Header-Percent'>
-                    {' '}
-                    {calculatePercentChange()}% to the previous day{' '}
-                  </div>
-                </div>
-
-                <div className='Admin-Account-Main-Header-Income col-md-3'>
-                  <div className='Admin-Account-Main-Header-Note'>
-                    {' '}
-                    Weekly income{' '}
-                  </div>
-                  <div className='Admin-Account-Main-Header-Money'> $0000 </div>
-                  <div className='Admin-Account-Main-Header-Percent'>
-                    {' '}
-                    6% day over week{' '}
-                  </div>
-                </div>
-
-                <div className='Admin-Account-Main-Header-Income col-md-3'>
-                  <div className='Admin-Account-Main-Header-Note'>
-                    {' '}
-                    Monthly income{' '}
-                  </div>
-                  <div className='Admin-Account-Main-Header-Money'> $0000 </div>
-                  <div className='Admin-Account-Main-Header-Percent'>
-                    {' '}
-                    3% day over month{' '}
-                  </div>
-                </div>
-
-                <div className='Admin-Account-Main-Header-Income col-md-3'>
-                  <div className='Admin-Account-Main-Header-Note'> Total </div>
-                  <div className='Admin-Account-Main-Header-Money'> $0000 </div>
-                  <div className='Admin-Account-Main-Header-Percent'>
-                    {' '}
-                    10% day over day{' '}
-                  </div>
-                </div>
-              </div>
-            )}
-
+            <Statistic />
             <div className='Admin-Account-Main-Table-Wrapper'>
               <div className='Admin-Account-Main-Table'>
                 <div className='Admin-Account-Main-Table-Title'>
