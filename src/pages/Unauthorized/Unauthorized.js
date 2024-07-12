36d81b96023390c3261c58dce037fff84beb4124
Minh <anhminh0026@gmail.com>
2024-07-12 23:19:07 +0700

    Unauthorized page

--- src/pages/Unauthorized/Unauthorized.js
+++ src/pages/Unauthorized/Unauthorized.js
@@ -1,5 +1,14 @@
-function Unauthorized() {
-  return <div></div>;
+import React from 'react';
+import './Unauthorized.css';
+
+const Unauthorized = () => {
+  return (
+    <div className="unauthorized-container">
+      <h1>401</h1>
+      <p>You are not authorized for selected action</p>
+      <a href="/login">Go to login page</a>
+    </div>
+  );
 }
 
 export default Unauthorized;
