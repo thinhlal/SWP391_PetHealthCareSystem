e598ef956febeb609bf9cf21b20fd50fab936697
Minh <anhminh0026@gmail.com>
2024-07-16 15:16:47 +0700

    navbar-doctor

--- src/components/Doctor/Header/Header.js
+++ src/components/Doctor/Header/Header.js
@@ -1,62 +1,35 @@
 //css
 import './Header.css';
-//img
+//css
 import logo from '../../../assets/images/Components/User/Header/logo.png';
 import userAccount from '../../../assets/images/Components/User/Header/img-7.png';
 import { useContext } from 'react';
 import { AuthContext } from '../../../context/AuthContext';
+
 function Header() {
   const { logOut, user } = useContext(AuthContext);
+  const pathname = window.location.pathname;
+
   return (
     <div className='main-header-doctor-overlap'>
       <div className='main-header-navigation'>
         <div>
-          <img
-            className='main-header-logo'
-            src={logo}
-            alt=''
-          />
+          <img className='main-header-logo' src={logo} alt='' />
+        </div>
+        <div className='navbar'>
+          <a className={`nav-item ${pathname === '/work-schedule' ? 'active' : ''}`} href='work-schedule'>Work Schedule</a>
+          <a className={`nav-item ${pathname === '/time-table' ? 'active' : ''}`} href='time-table'>Working Time</a>
+          <a className={`nav-item ${pathname === '/doctor-care' ? 'active' : ''}`} href='doctor-care'>Doctor Care</a>
         </div>
         <div className='main-header-user_wrapper'>
           <div className='main-header-user'>
             <div className='main-header-user-account'>
-              <img
-                className='account_icon'
-                src={userAccount}
-                alt=''
-              />
+              <img className='account_icon' src={userAccount} alt='' />
               <div className='main-header-user-account-name'>
                 Hi {user?.doctorDetails[0]?.name}
               </div>
               <div className='main-header-user-account_menu'>
-                <a
-                  className='forward'
-                  href='work-schedule'
-                >
-                  <div className='main-header-user-account_menu-item'>
-                    Work Schedule
-                  </div>
-                </a>
-                <a
-                  className='forward'
-                  href='time-table'
-                >
-                  <div className='main-header-user-account_menu-item'>
-                    Working Time
-                  </div>
-                </a>
-                <a
-                  className='forward'
-                  href='doctor-care'
-                >
-                  <div className='main-header-user-account_menu-item'>
-                    Doctor Care
-                  </div>
-                </a>
-                <div
-                  className='main-header-user-account_menu-item'
-                  onClick={logOut}
-                >
+                <div className='main-header-user-account_menu-item' onClick={logOut}>
                   Log out
                 </div>
               </div>
