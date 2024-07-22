aec4f77fd07706496462440bafe2c39114768f0f
Minh <anhminh0026@gmail.com>
2024-07-22 23:20:27 +0700

    button login + sign up

--- src/components/User/Header/Header.js
+++ src/components/User/Header/Header.js
@@ -1,9 +1,11 @@
 //css
 import './Header.css';
 import { useContext } from 'react';
+import { Link } from 'react-router-dom'; // Import Link component
 //img
 import logo from '../../../assets/images/Components/User/Header/logo.png';
 import { AuthContext } from '../../../context/AuthContext';
+
 function Header() {
   const { user, logOut } = useContext(AuthContext);
   return (
@@ -49,44 +51,51 @@ function Header() {
           </div>
         </div>
         <div className='main-header-user_wrapper'>
-          {user && <div className='main-header-user'>
-            <div className='main-header-user-account'>
-              <img
-                className='account_icon'
-                src={user?.customerDetails[0]?.image}
-                alt=''
-              />
-              <div className='main-header-user-account-name'>
-                Hi {user?.customerDetails[0]?.name}
-              </div>
-              <div className='main-header-user-account_menu'>
-                <a
-                  href='your-booking'
-                  className='main-header-user-account_menu-item'
-                >
-                  Your Bookings
-                </a>
-                <a
-                  href='your-pet'
-                  className='main-header-user-account_menu-item'
-                >
-                  View Your Pet
-                </a>
-                <a
-                  href='user-profile'
-                  className='main-header-user-account_menu-item'
-                >
-                  Settings
-                </a>
-                <div
-                  onClick={logOut}
-                  className='main-header-user-account_menu-item'
-                >
-                  Log out
+          {user ? (
+            <div className='main-header-user'>
+              <div className='main-header-user-account'>
+                <img
+                  className='account_icon'
+                  src={user?.customerDetails[0]?.image}
+                  alt=''
+                />
+                <div className='main-header-user-account-name'>
+                  Hi {user?.customerDetails[0]?.name}
+                </div>
+                <div className='main-header-user-account_menu'>
+                  <a
+                    href='your-booking'
+                    className='main-header-user-account_menu-item'
+                  >
+                    Your Bookings
+                  </a>
+                  <a
+                    href='your-pet'
+                    className='main-header-user-account_menu-item'
+                  >
+                    View Your Pet
+                  </a>
+                  <a
+                    href='user-profile'
+                    className='main-header-user-account_menu-item'
+                  >
+                    Settings
+                  </a>
+                  <div
+                    onClick={logOut}
+                    className='main-header-user-account_menu-item'
+                  >
+                    Log out
+                  </div>
                 </div>
               </div>
             </div>
-          </div>}
+          ) : (
+            <div className='auth-buttons'>
+              <Link to='/login' className='auth-button'>Login</Link>
+              <Link to='/signup' className='auth-button'>Sign Up</Link>
+            </div>
+          )}
         </div>
       </div>
     </div>
